aa78c085a696b0d90234f5e2ccc1f5a1e63c34dd
root <root@ip-172-31-21-79.eu-north-1.compute.internal>
2023-11-29 19:36:06 +0000

    new folder and files added

--- backend.sh
+++ backend.sh
@@ -4,7 +4,7 @@
 USERNAME="Mubashir2022skipq"
 TOKEN="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"
 
-# Replace 'REPO_OWNER' and 'REPO_NAME' with the owner and name of your GitHub repository.
+
 REPO_OWNER="https://github.com/Mubashir2022skipq/Deploy-Application-todo.git"
 REPO_NAME="Deploy-Application-todo"
 
@@ -16,15 +16,11 @@ mkdir backend
 touch backend.py
 
 
-
 # Add all files to the staging area
 git add .
 
 # Commit changes
 git commit -m "new folder and files added"
 
-# Set the remote repository URL with the token
-git remote add origin "https://${USERNAME}:${TOKEN}@github.com/${REPO_OWNER}/${REPO_NAME}.git"
-
-# Push to the main branch
-git push origin main
+# Push to the main branch, including the repository URL and token in the URL
+git push "https://${USERNAME}:${TOKEN}@github.com/${REPO_OWNER}/${REPO_NAME}.git" main
